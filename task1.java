abb26ae043b86d0a659512fc572c99e097b7a1d1
urvimandani <135199854+urvimandani@users.noreply.github.com>
2023-09-07 12:51:25 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,32 @@
+
+public class task1 {
+ 
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Enter s1 marks");
+		int s1 = 66;
+		
+		System.out.println("Enter S2  marks");
+		int s2 = 43;
+		
+		System.out.println("Enter s3 marks");
+		int s3 = 74;
+		
+		System.out.println("Enter s4 marks");
+		int s4 = 85;
+		
+		System.out.println("Enter s5 marks");
+		int s5 = 45;
+		
+		System.out.println(s1+s2+s3+s4+s5);
+		
+		System.out.println(s1%s2%s3%s4%s5);
+	}
+		
+		
+		
+		
+		
+	}
+
